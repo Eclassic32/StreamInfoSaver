09079aa7c591ad374286995fe0d0eb5cc3da21ec
Eclassic32 <eclassic32004@gmail.com>
2025-06-02 14:39:44 +0500

    Attempt 2

---
+++ newUI/twitch.js
@@ -0,0 +1,87 @@
+// Sample category data
+const sampleCategories = [
+    {
+        id: 1,
+        title: "Just Chatting",
+        viewers: "185K",
+        followers: "31.1M",
+        tags: ["IRL"],
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/509658-40x53.jpg"
+    },
+    {
+        id: 2,
+        title: "Just Dance 2025 Edition",
+        viewers: "14",
+        followers: "2K",
+        tags: ["Rythm & Music Game", "Party"],
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/516575-40x53.jpg"
+    },
+    {
+        id: 3,
+        title: "Just Another Night Shift",
+        viewers: "",
+        followers: "77",
+        tags: ["Simulation", "Indie Game", "Action"],
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/1469308723-40x53.jpg"
+    },
+    {
+        id: 4,
+        title: "Just Cating",
+        viewers: "26",
+        followers: "1.8K",
+        tags: ["IRL"],   
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/509658-40x53.jpg"
+    },
+    {
+        id: 5,
+        title: "Just Dance",
+        viewers: "",
+        followers: "28.5K",
+        tags: ["Rythm & Music Game", "Party"],
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/33214-40x53.jpg"
+    },
+    {
+        id: 6,
+        title: "Just Dance 2022",
+        viewers: "",
+        followers: "113K",
+        tags: ["Rythm & Music Game", "Party"],
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/518012-40x53.jpg"
+    },
+    {
+        id: 7,
+        title: "Minecraft",
+        viewers: "89.2K",
+        followers: "47.3M",
+        tags: ["Simulation", "Adventure"],
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-40x53.jpg"
+    },
+    {
+        id: 8,
+        title: "League of Legends",
+        viewers: "234K",
+        followers: "67.8M",
+        tags: ["RPG", "Strategy", "MOBA", "Action"],
+        image: "https://static-cdn.jtvnw.net/ttv-boxart/21779-40x53.jpg"
+    }
+];
+
+// Sample tag suggestions
+const sampleTagSuggestions = [
+    "MinecraftFreizeitpark",
+    "minecraftplays", 
+    "min",
+    "minecraftchillplay",
+    "minaashidofan",
+    "minecraftgaminglive",
+    "MinecraftPY",
+    "minecraft",
+    "gaming",
+    "chill",
+    "chatting",
+    "english",
+    "fun",
+    "community",
+    "interactive",
+    "wholesome"
+];
